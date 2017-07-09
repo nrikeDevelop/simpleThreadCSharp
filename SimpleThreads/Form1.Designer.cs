0e5ccc6a371ccf30919c2dc51a05bb6d905d5290
nrikeDevelop <nrikedel96.develop@gmail.com>
2017-07-09 16:14:46 +0200

    ok

---
+++ SimpleThreads/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SimpleThreads
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.threadTwo = new System.Windows.Forms.Button();
+            this.threadOne = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // threadTwo
+            // 
+            this.threadTwo.Location = new System.Drawing.Point(12, 109);
+            this.threadTwo.Name = "threadTwo";
+            this.threadTwo.Size = new System.Drawing.Size(127, 23);
+            this.threadTwo.TabIndex = 0;
+            this.threadTwo.Text = "Thread 2";
+            this.threadTwo.UseVisualStyleBackColor = true;
+            this.threadTwo.Click += new System.EventHandler(this.threadTwo_Click);
+            // 
+            // threadOne
+            // 
+            this.threadOne.Location = new System.Drawing.Point(12, 80);
+            this.threadOne.Name = "threadOne";
+            this.threadOne.Size = new System.Drawing.Size(127, 23);
+            this.threadOne.TabIndex = 1;
+            this.threadOne.Text = "Thread 1";
+            this.threadOne.UseVisualStyleBackColor = true;
+            this.threadOne.Click += new System.EventHandler(this.threadOne_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mensaje a mostrar";
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(12, 37);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(122, 20);
+            this.txt.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(149, 143);
+            this.ControlBox = false;
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.threadOne);
+            this.Controls.Add(this.threadTwo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button threadTwo;
+        private System.Windows.Forms.Button threadOne;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt;
+    }
+}
+
---
+++ SimpleThreads/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace SimpleThreads
+{
+    public partial class Form1 : Form
+    {
+
+        String text;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void threadOne_Click(object sender, EventArgs e)
+        {
+            if (getText())
+            {
+                new Thread(() =>
+                {
+                    Thread.CurrentThread.IsBackground = true;
+                    MessageBox.Show("Tread one "+text);
+                
+                }).Start();
+            }
+        }
+
+        private void threadTwo_Click(object sender, EventArgs e)
+        {
+            if (getText())
+            {
+                Thread t = new Thread(MyThread);
+                t.Start();
+            }
+        }
+
+        public void MyThread()
+        {
+            MessageBox.Show("Thread two "+text);
+        }
+
+        public Boolean getText()
+        {
+            if (txt.Text.Equals(""))
+            {
+                MessageBox.Show("Introduce algun texto");
+                return false;
+            }
+            else
+            {
+                text = txt.Text;
+                return true;
+            }
+        }
+    }
+}
